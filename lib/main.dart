17a62f184d09df57d729a70695342fad94bd9175
Rijan77 <rijanacharya73@gmail.com>
2024-11-12 22:45:28 +0545

    Create splash screen with timer and logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:finance_mate/pages/Onboarding%20Screen.dart';
+import 'package:finance_mate/pages/SplashScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  OnboardingPage(),
+      home:  Splashscreen(),
     );
   }
 }
---
+++ lib/pages/SplashScreen.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:finance_mate/pages/Onboarding%20Screen.dart';
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to the onboarding screen after a delay
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement<void, void>(
+        context,
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) => const OnboardingPage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("lib/assets/Logo.png", height: 350, width: 350),
+            SizedBox(height: 30),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
